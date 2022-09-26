a56a8c460c88b3fe5d44c2ad58f5f9bffbaabb23
akumar23 <kumar.aryan@gmail.com>
2022-09-26 04:53:47 +0000

    added loop and counter

--- guess_num/src/main.rs
+++ guess_num/src/main.rs
@@ -1,26 +1,35 @@
 use std::io;
 use rand::Rng;
-
+use std::cmp::Ordering;
 
 fn main() {
     println!("Guess a number between 1-100");
-
-    println!("Please input guess");
-    let mut guess = String::new();
-
-    io::stdin()
-        .read_line(&mut guess)
-        .expect("Failed to read line");
-    
-    let guess_num: i32 = guess.trim().parse().expect("Input isn't an integer");
     
     //generate random number 
     let num = rand::thread_rng().gen_range(0..100);
+    
+    let mut count = 0;
 
-    if num == guess_num {
-        println!("You guessed right!! The number was: {guess}");
-    } else {
-        println!("Sorry you guessed wrong. The number was: {}", num);
+    loop {
+
+        println!("Please input guess");
+        let mut guess = String::new();
+        count += 1;
+
+        io::stdin()
+            .read_line(&mut guess)
+            .expect("Failed to read line");
+
+        let guess_num: i32 = guess.trim().parse()
+                                    .expect("Input isn't an number");
+
+        match guess_num.cmp(&num) {
+            Ordering::Less => println!("Too small"),
+            Ordering::Greater => println!("Too big!"),
+            Ordering::Equal => {
+                println!("You win! It took you {count} guesses!");
+                break;
+            }
+        }
     }
-
 }
