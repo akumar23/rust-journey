afe1d1457fb76d4a1db73bbcd67880337f7da311
akumar23 <kumar.aryan@gmail.com>
2022-10-15 21:32:15 +0000

    pin should turn on and off with a loop

--- led_light/src/main.rs
+++ led_light/src/main.rs
@@ -2,10 +2,36 @@
 #![no_main]
 
 use core::panic::PanicInfo;
+use core::arch::asm;
+
+mod boot {
+    use core::arch::global_asm;
+
+    global_asm!(
+        ".section .text._start"
+    );
+}
 
 #[no_mangle]
 pub extern "C" fn _start() -> ! {
-    loop {}
+    unsafe {
+        core::ptr::write_volatile(0x3F20_0008 as *mut u32, 1<<3);
+
+        loop {
+            core::ptr::write_volatile(0x3F20_001C as *mut u32, 1<<21);
+            
+            for _ in 1..50000 {
+                asm!("nop");
+            }
+
+            core::ptr::write_volatile(0x3F20_0028 as *mut u32, 1<<21);
+
+            for _ in 1..50000 {
+                asm!("nop");
+            }
+        }
+
+    }
 
 }
 
